1eb509d653b80d34a22a5dd46362d00bd232705f
MimoriSeica <zerons_0612@yahoo.co.jp>
2019-10-19 06:30:25 +0900

    プロット

--- plot_rmse.py
+++ plot_rmse.py
@@ -18,12 +18,41 @@ def read_json_file(path):
     with open(path,'r') as f:
         return json.load(f)
 
+def get_wham_data():
+    with open("wham_from_cycle_data.txt") as file:
+        return np.array([str.strip().split() for str in file.readlines()], dtype = 'float')[:, 1]
+
 def main():
-    rmse_list = read_json_file("rmse_list.json")["rmse_list"]
+    row_datas_gp = read_json_file("row_data.json")
+    wham_data = get_wham_data()
+    rmse_list_gp = []
+
+    for count in range(100):
+        free_enegy = row_datas_gp["free_enegy"]["{}".format(count)]
+        sum = 0;
+
+        for i in range(BIN_SIZE):
+            sum += (free_enegy[i] - wham_data[i]) ** 2
+
+        rmse_list_gp.append(sum / BIN_SIZE)
+
+    row_datas_liner = read_json_file("row_data_liner.json")
+    rmse_list_liner = []
+
+    for count in range(100):
+        free_enegy = row_datas_liner["free_enegy"]["{}".format(count)]
+        sum = 0;
+
+        for i in range(BIN_SIZE):
+            sum += (free_enegy[i] - wham_data[i]) ** 2
+
+        rmse_list_liner.append(sum / BIN_SIZE)
 
     f, ax = plt.subplots(1, 1, figsize=(8, 6))
-    ax.plot(range(len(rmse_list)), rmse_list, 'b')
-    ax.plot(range(len(rmse_list)), [1.210058043189025 for i in range(len(rmse_list))], 'b', color = "red")
+    ax.plot(range(len(rmse_list_gp)), rmse_list_gp, 'b')
+    ax.plot(range(len(rmse_list_liner)), rmse_list_liner, color = "red")
+    ax.plot(range(len(rmse_list_gp)), [0.1909531092466414 for i in range(len(rmse_list_gp))], color = "orange")
+    ax.plot(range(len(rmse_list_gp)), [2.6104119312491014 for i in range(len(rmse_list_gp))])
     ax.set_ylim([0, 10])
     ax.set_xlabel("cycle count")
     ax.set_ylabel("RMSE")
